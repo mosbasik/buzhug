218b4fc6b737ff778c46357b6bf7f5f74a9953d1
David Warrick <dwarrick@caltech.edu>
2015-04-24 23:03:58 -0700

    Initial completion of Create Table parser capability.
    The code isn't pretty, but it works for the rudimentary test case of creating a table with a single 'int' column.

--- javasrc/SqlParser.java
+++ javasrc/SqlParser.java
@@ -6,14 +6,16 @@ import org.python.core.*;
 
 import java.sql.SQLException;
 import java.util.Vector;
+import java.util.ArrayList;
 import java.io.*;
 
 import org.gibello.zql.*;
 import org.gibello.zql.data.*;
 
 public class SqlParser {
+    private static String basePath = "/home/david/CS123/buzhug/javasrc/tables/";
+
     private static PythonInterpreter interpreter = new PythonInterpreter(null, new PySystemState());
-    //interp = new PythonInterpreter(null, new PySystemState());
 
     
     public static void main(String args[]) {
@@ -99,9 +101,10 @@ public class SqlParser {
                 if (firstWord.equalsIgnoreCase("Create")) {
                     callCreate(fullCommand);
                 }
-                else if (firstWord.equalsIgnoreCase("Drop")) {
+                // Drop isn't actually supported by buzhug?!
+                /*else if (firstWord.equalsIgnoreCase("Drop")) {
                     callDrop(fullCommand);
-                }
+                }*/
                 else {
                     // Create a ZqlParser from fullCommand
                     try {
@@ -151,7 +154,176 @@ public class SqlParser {
     }
     
     private static void callCreate(String st) {
-        System.out.println("Create table not yet supported!");
+        //System.out.println("Create table not yet supported!");
+        // Get the table name
+        String tableName = "";
+        while (tableName.length() == 0) {
+            int nextSpace = st.indexOf(' ');
+            if (nextSpace == -1) {
+                System.out.println("Unable to create table: table name not"
+                                    + " found!");
+                return;
+            }
+            String tmp = st.substring(0, nextSpace);
+            st = st.substring(nextSpace + 1);
+            st.trim();
+            if(!tmp.equalsIgnoreCase("Create") && !tmp.equalsIgnoreCase("Table")) {
+                tableName = tmp;
+            }
+        }
+        // Get the opening paren so we know where to start
+        int openParen = st.indexOf('(');
+        if (openParen == -1) {
+            System.out.println("Unable to create table: bad syntax!");
+            return;
+        }
+        st = st.substring(openParen + 1);
+        st = st.trim();
+        
+        // Begin getting the column names and column types
+        ArrayList<String> columnNames = new ArrayList<String>();
+        ArrayList<String> columnTypes = new ArrayList<String>();
+        ArrayList<String> columnDefaults = new ArrayList<String>();
+        while (st.length() != 0) {
+            // Get the column name
+            int spaceLoc = st.indexOf(' ');
+            if (spaceLoc == -1) {
+                System.out.println("Unable to create table: parsing error!");
+                return;
+            }
+            columnNames.add(st.substring(0, spaceLoc));
+            st = st.substring(spaceLoc);
+            st = st.trim();
+            
+            // Get the column type
+            String colTypePotential = "";
+            int commaLoc = st.indexOf(',');
+            if (commaLoc == -1) {
+                //We must be at the end of the table. Get the last paren instead.
+                commaLoc = st.lastIndexOf(')');
+                if (commaLoc == -1) {
+                    //If this doesn't exist, then there's really a problem.
+                    System.out.println("Unable to create table: parsing error!");
+                    return;
+                }
+                else {
+                    colTypePotential = st.substring(0, commaLoc);
+                    st = "";
+                }
+            }
+            else {
+                colTypePotential = st.substring(0, commaLoc);
+                st = st.substring(commaLoc+1);
+                st = st.trim();
+            }
+            colTypePotential = colTypePotential.trim();
+            
+            // Get the actual column type from colTypePotential
+            spaceLoc = colTypePotential.indexOf(' ');
+            String colType = "";
+            if (spaceLoc == -1) {
+                //There's no extra information; set colType
+                colType = colTypePotential;
+            }
+            else {
+                colType = colTypePotential.substring(0,spaceLoc);
+            }
+            
+            // if there is a default value, get it. Otherwise, ignore all other
+            // keywords as buzhug doesn't support them
+            String defaultVal = "";
+            while(colTypePotential.length() > 9) {
+                //"Default" is 7-characters long
+                String testString = colTypePotential.substring(0,7);
+                if(testString.equalsIgnoreCase("Default")) {
+                    colTypePotential = colTypePotential.substring(7);
+                    colTypePotential = colTypePotential.trim();
+                    // Get the col type; it is the next value in the string
+                    spaceLoc = colTypePotential.indexOf(' ');
+                    if (spaceLoc == -1) {
+                        defaultVal = colTypePotential;
+                    }
+                    else {
+                        defaultVal = colTypePotential.substring(0, spaceLoc);
+                    }
+                    break;
+                }
+                else {
+                    colTypePotential = colTypePotential.substring(1);
+                }
+            }
+            columnDefaults.add(defaultVal);
+            
+            // buzhug only supports str, unicode, int, float, bool, date, or
+            // datetime column types. Technically it also supports "links",
+            // but these are basically equivalent to foreign keys, which we can
+            // deal with once we implement joins (if we implement joins...)
+            // It may not be standard sql, strictly speaking, but if a column
+            // type is not one of the above then we will ignore it and print
+            // an error.
+            if(!colType.equalsIgnoreCase("str") && 
+               !colType.equalsIgnoreCase("unicode") &&
+               !colType.equalsIgnoreCase("int") &&
+               !colType.equalsIgnoreCase("float") &&
+               !colType.equalsIgnoreCase("bool") &&
+               !colType.equalsIgnoreCase("date") &&
+               !colType.equalsIgnoreCase("datetime")) {
+                System.out.println("Buzhug only supports the following column"
+                                    + " types:");
+                System.out.println("\tstr");
+                System.out.println("\tunicode");
+                System.out.println("\tint");
+                System.out.println("\tfloat");
+                System.out.println("\tbool");
+                System.out.println("\tdate");
+                System.out.println("\tdatetime");
+                System.out.println("Can't create table.");
+                System.out.println("  colType: " + colType);
+                return;
+            }
+            else {
+                columnTypes.add(colType);
+            }
+        }
+        
+        // Ensure that the three arrayLists are the same size, or there is a
+        // problem
+        if (columnTypes.size() != columnDefaults.size() ||
+            columnTypes.size() != columnNames.size() ||
+            columnNames.size() != columnDefaults.size()) {
+            System.out.println("Unable to create table: number of columns types"
+                                + ", names, or defaults is not equal!");
+            return;
+        }
+        
+        // Create the buzhug-formatted string
+        String buzhugCreate1 = tableName + " = Base('" + basePath + tableName + "')";;
+        String buzhugCreate2 = tableName + ".create(";
+        for (int i = 0; i < columnNames.size(); i++) {
+            String colEntry = "('" + columnNames.get(i) +"', "+columnTypes.get(i);
+            if (columnDefaults.get(i).length() != 0) {
+                colEntry = colEntry + ", " + columnDefaults.get(i) + ")";
+            }
+            else {
+                colEntry = colEntry + ")";
+            }
+            buzhugCreate2 = buzhugCreate2 + colEntry + ", ";
+        }
+        buzhugCreate2 = buzhugCreate2.substring(0,buzhugCreate2.length()-2) + ")";
+        
+        // Debugging purposes
+        System.out.println("Formated statement:\n" + buzhugCreate2);
+        
+        // call buzhug.
+        try {
+            interpreter.exec(buzhugCreate1);
+            interpreter.exec(buzhugCreate2);
+        }
+        catch (Exception e) {
+            System.out.println("Python Interpreter exception: " + e.getMessage());
+            System.out.println("Stacktrace:");
+            e.printStackTrace();
+        }
     }
     
     private static void callDrop(String st) {
@@ -198,8 +370,16 @@ public class SqlParser {
         System.out.println("Formated statement:\n" + buzhugInsert);
         
         // call buzhug.
-        interpreter.exec(st.getTable() + " = Base('/home/david/CS123/buzhug/javasrc/tables').open()");
-        interpreter.exec(buzhugInsert);
+        try {
+            interpreter.exec(st.getTable() + " = Base('" + basePath 
+                             + st.getTable() + "').open()");
+            interpreter.exec(buzhugInsert);
+        }
+        catch (Exception e) {
+            System.out.println("Python Interpreter exception: " + e.getMessage());
+            System.out.println("Stacktrace:");
+            e.printStackTrace();
+        }
     }
     
     private static void callUpdate(ZUpdate st) {
