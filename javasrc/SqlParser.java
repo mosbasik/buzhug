bbfc568281a3f437a03a0c11d0ef8e853ec8603c
Sharjeel Aziz <saziz@caltech.edu>
2015-04-25 13:26:53 -0700

    Added Update and select

--- javasrc/SqlParser.java
+++ javasrc/SqlParser.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.Vector;
 import java.io.*;
 
+import java.util.*;
 import org.gibello.zql.*;
 import org.gibello.zql.data.*;
 
@@ -25,8 +26,16 @@ public class SqlParser {
         try {
             System.out.println("Preparing buzhug environment.");
             PySystemState sys = Py.getSystemState();
+//            sys.path.append(new PyString("/Users/saziz/jython2.7rc3/jython.jar"));
+//            sys.path.append(new PyString("/Users/saziz/Desktop/cs123/buzhug/buzhug"));
+//            System.out.println(sys);
+
             sys.path.append(new PyString("/usr/java/Jython/jython.jar"));
             sys.path.append(new PyString("/home/david/CS123/buzhug/buzhug"));
+
+            interpreter.exec("import sys");
+            interpreter.exec("print sys.version_info");
+
             interpreter.exec("import buzhug");
             interpreter.exec("from buzhug import Base");
             ZqlParser p = null;
@@ -53,7 +62,8 @@ public class SqlParser {
                     callInsert((ZInsert)st);
                 }
                 else if (st instanceof ZUpdate) {
-                    System.out.println("Update not yet supported!");
+                    callUpdate((ZUpdate) st);
+                    //System.out.println("Update not yet supported!");
                     // TODO: call buzhug update with the parsed inputs
                 }
                 else if (st instanceof ZDelete) {
@@ -61,7 +71,8 @@ public class SqlParser {
                     // TODO: call buzhug delete with the parsed inputs
                 }
                 else if (st instanceof ZQuery) {
-                    System.out.println("Select not yet supported!");
+                    callSelect((ZQuery)st);
+//                    System.out.println("Select not yet supported!");
                     // TODO: call buzhug select with the parsed inputs
                 }
             }
@@ -113,12 +124,67 @@ public class SqlParser {
     }
     
     private static void callUpdate(ZUpdate st) {
+        String table = st.getTable();
+        // i think this is how tables are opened? Not sure since I can't execute...
+        // loading the table
+        interpreter.exec("temp = Base('/home/david/CS123/buzhug/javasrc/tables/'"+table+".db).open()");
+        // finding the records to update
+        String cmd1 = "recs = temp.select_for_update(None, " + st.getWhere() + ")";
+        interpreter.exec(cmd1);
+        // updating
+        String cmd2 = table + ".update(recs, ";
+        HashSet vals = st.getSet();
+        for(String col:vals.keys())
+        {
+            cmd2+=col+"="+vals.get(col).toString()+",";
+        }
+        cmd2 = cmd2.substring(0, cmd2.length()-1) + ")";
+        interpreter.exec(cmd2);
     }
     
     private static void callDelete(ZDelete st) {
+
     }
     
     private static void callSelect(ZQuery st) {
+        // result_set holds the selected
+        String cmd = "result_set = ";
+        // make sure its a simple select
+        if(st.getFrom().contains("join"))
+        {
+            System.out.println("Only simple joins work");
+            return;
+        }
+        //  its a *(i have to check what's in getSelect() when its a star)
+        // can't run at the moment, so im assuming null
+        if(st.getSelect() == null)
+        {
+            cmd += "db.select(None)";
+        }
+        // if it's not a star, simply go through and find the columns
+        // we're looking for
+        else
+        {
+            Vector columns = st.getSelect();
+            String values = "[";
+            for (int i = 0; i < columns.size(); i++) {
+                values = values + columns.get(i) + ",";
+            }
+            values = values.substring(0, values.length()-1);
+            values += ']';
+            cmd += "db.select("+values + ")";
+        }
+        // if there is a where, we add it
+        if(st.getWhere() != null)
+        {
+            cmd = cmd.substring(0, cmd.length() - 1);
+            cmd += "," + st.getWhere().toString()+")";
+        }
+        // run on python
+        interpreter.exec("db = Base('/home/david/CS123/buzhug/javasrc/tables/"+st.getFrom().toString()+"').open()");
+        interpreter.exec(cmd);
+        interpreter.exec("for a in result_set:");
+        interpreter.exec("\tprint a");
     }
     
     // Returns the buzhug-formated select statement generated from st.
